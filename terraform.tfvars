909617a5f889696747601b31f95e00aec6bc3efc
Pratyushaa94 <prathyusha9449@gmail.com>
2025-07-07 19:04:23 +0530

    changes in yml

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
 aws_region  = "us-east-1"
 bucket_name = "prathyusha733714"
+environment = "dev"
--- variables.tf
+++ variables.tf
@@ -8,3 +8,8 @@ variable "bucket_name" {
   description = "Unique S3 bucket name"
   type        = string
 }
+
+variable "environment" {
+  description = "Environment name like dev or prod"
+  type        = string
+}
